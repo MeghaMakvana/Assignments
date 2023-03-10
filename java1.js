f364d37292063ff423db76e1ce7329ea5dbffa19
MeghaMakvana <127474844+MeghaMakvana@users.noreply.github.com>
2023-03-10 12:03:11 +0530

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,24 @@
+console.log(document.title);
+document.title="WELCOME MEGHA";
+
+let hd = document.querySelector("h1");
+
+hd.textContent = "welcome to MaiN page";
+
+hd.style.color="teal";
+hd.style.backgroundColor="darkred";
+ 
+hd.style.fontSize="100px";
+
+console.warn("this is warning");
+
+
+let nm = document.getElementById('h');
+nm.textContent ="MOM";
+
+let mm = document.getElementById('m');
+mm.textContent ="papa";
+window.alert("hello world");
+console.error("this is error");
+window.prompt("name");
+console.dir("this is directory");
